597a6256779602041d99857e20f438aeb382c4ff
Fukui-S <tibita13579@yahoo.co.jp>
2021-04-06 08:41:49 +0000

    [Finish]4-3

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '9d2d4b22d908630b4849cd108c33d131f5df2a4e2f6d02f3f8bcb6fc40744f39b8867606b21566b9aeacaba1c7434b490cd5e8951b97ed0b00fa8bc9d2f1f078'
